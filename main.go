ae29b224c87c6e546aee3c097a46ac2781a61c46
RatNexus <michal.szalachowski.33@gmail.com>
2025-08-14 09:19:53 +0200

    feat: add CLI, no crawling yet

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	if len(os.Args) <= 1 {
+		fmt.Println("no website provided")
+		os.Exit(1)
+	}
+
+	if len(os.Args) != 2 {
+		fmt.Println("too many arguments provided")
+		os.Exit(1)
+	}
+	baseUrl := os.Args[1]
+	fmt.Printf("starting crawl of: %s\n", baseUrl)
+}
