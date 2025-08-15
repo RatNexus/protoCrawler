945466a7b0fc2289463fb7e5ada11b5a6f0dede6
RatNexus <michal.szalachowski.33@gmail.com>
2025-08-15 15:45:02 +0200

    feat: add concurrency

--- crawl_page.go
+++ crawl_page.go
@@ -2,67 +2,163 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"log"
 	"net/url"
+	"os"
 )
 
 func resolveRelativeURLs() {}
 
-// Todo: add resoleRelativeURLs here
-func crawlPage(rawBaseURL, rawCurrentURL string, pages map[string]int, currentDepth, maxDepth int) {
-	currentDepth += 1
-	if maxDepth > 0 && currentDepth > maxDepth {
-		//log.Printf("   abyss: \"%s\"\n", rawCurrentURL)
-		return
-	}
-	log.Printf(" - DEPTH %d\n", currentDepth)
+func (cfg *config) addPageVisit(normalizedURL string) (isFirst bool) { return }
 
-	// enforce same host for both input URLs
-	parsedBaseUrl, err := url.Parse(rawBaseURL)
-	if err != nil {
-		log.Println("Base url parse error: ", err)
-		return
+func logError(message, rawCurrentURL string, err error) {
+	log.Printf(" - error  : \"%s\" :ERROR: %s%s\n", rawCurrentURL, message, err)
+}
+
+func (cfg *config) crawlPage(rawCurrentURL string) {
+	var currentDepth uint // == 0 because go i awesome
+	//var pageCount int32
+	//pageCount += 1
+
+	if cfg.lo.logToFile {
+		cfg.lf = cfg.lo.getLogFile()
+		defer cfg.lf.Close()
 	}
+
+	if cfg.lo.logToScreen && cfg.lo.logToFile {
+		multiWriter := io.MultiWriter(os.Stdout, cfg.lf)
+		log.SetOutput(multiWriter)
+	} else if cfg.lo.logToScreen {
+		log.SetOutput(os.Stdout)
+	} else if cfg.lo.logToFile {
+		log.SetOutput(cfg.lf)
+	} else {
+		log.SetOutput(io.Discard)
+	}
+
+	if cfg.lo.doLogging && cfg.lo.doStart {
+		// make this message variable depending on other logging options
+		log.Printf("starting crawl of: \"%s\" with max depth of %d\n",
+			cfg.baseURL, cfg.maxDepth)
+		log.Print("\n----------\n\n")
+	}
+
+	cfg.concurrencyControl <- struct{}{}
+	cfg.wg.Add(1)
+	go cfg.internalCrawlPage(rawCurrentURL, currentDepth)
+	cfg.wg.Wait()
+
+	if cfg.lo.doLogging && cfg.lo.doEnd {
+		log.Print("\n----------\n\n")
+		log.Println("end of crawl")
+		log.Print("\n----------\n\n")
+	}
+
+	// Todo: this should be a separate function
+	if cfg.lo.doLogging && cfg.lo.doSummary {
+		log.Println("Contents of the pages map:")
+		cfg.mu.Lock() // log.Print is slower than making a map copy
+		pages := make(map[string]int)
+		for url, n := range cfg.pages {
+			pages[url] = n
+		}
+		cfg.mu.Unlock()
+
+		// thus no lock and direct use of cfg.pages here
+		for url, n := range pages {
+			log.Printf("%s ||| N: %d\n", url, n)
+		}
+	}
+
+}
+
+// Todo: add resoleRelativeURLs here
+func (cfg *config) internalCrawlPage(rawCurrentURL string, currentDepth uint) {
+	defer cfg.wg.Done()
+	defer func() { <-cfg.concurrencyControl }()
+	currentDepth += 1
+
+	// enforce same host for base and current URLs, Part 1
 	parsedCurrentUrl, err := url.Parse(rawCurrentURL)
 	if err != nil {
-		log.Println("Current url parse error: ", err)
+		if cfg.lo.doLogging && cfg.lo.doErrors {
+			logError("current url parse error: ", rawCurrentURL, err)
+		}
 		return
 	}
 
 	// normalise current URL
 	currentUrl, err := normalizeURL(rawCurrentURL)
 	if err != nil {
-		log.Println("Normalise current url error: ", err)
+		if cfg.lo.doLogging && cfg.lo.doErrors {
+			logError("normalise current url error: ", rawCurrentURL, err)
+		}
 		return
 	}
 
 	// increment or expand pages map of current URL
-	_, exists := pages[currentUrl]
-	pages[currentUrl]++
-	if exists {
+	cfg.mu.Lock()
+	_, exists := cfg.pages[currentUrl]
+	cfg.pages[currentUrl]++
+	cfg.mu.Unlock()
+
+	if cfg.maxDepth > 0 && currentDepth > cfg.maxDepth {
+		if cfg.lo.doLogging && cfg.lo.doAbyss {
+			log.Printf("   abyss: \"%s\"\n", rawCurrentURL)
+		}
 		return
 	}
 
-	if parsedBaseUrl.Host != parsedCurrentUrl.Host {
-		log.Println(fmt.Printf(
-			"Host difference: Base: \"%s\", Current: \"%s\"\n",
-			parsedBaseUrl.Host, parsedCurrentUrl.Host))
+	doDepthLog := cfg.lo.doLogging && cfg.lo.doDepth
+	if exists {
+		if doDepthLog {
+			log.Printf(" - depth %d: \"%s\"\n", currentDepth, rawCurrentURL)
+		}
+		return
+	} else {
+		if doDepthLog {
+			log.Printf(" - DEPTH %d: \"%s\"\n", currentDepth, rawCurrentURL)
+		}
+	}
+
+	// enforce same host for base and current URLs, Part 2
+	if cfg.baseURL.Host != parsedCurrentUrl.Host {
+		if cfg.lo.doLogging && cfg.lo.doErrors {
+			err := fmt.Errorf(
+				"host difference: Base: \"%s\", Current: \"%s\"",
+				cfg.baseURL.Host, parsedCurrentUrl.Host)
+			logError("", rawCurrentURL, err)
+		}
 		return
 	}
 
 	// parse links of current webpage
 	webpage, err := getHTML(rawCurrentURL)
 	if err != nil {
-		log.Println("getHTML error: ", err)
+		if cfg.lo.doLogging && cfg.lo.doErrors {
+			logError("getHTML error: ", rawCurrentURL, err)
+		}
+		return
 	}
 	gotURLs, err := getURLsFromHTML(webpage, rawCurrentURL)
 	if err != nil {
-		log.Println("getURLsFromHTML error: ", err)
+		if cfg.lo.doLogging && cfg.lo.doErrors {
+			logError("getURLsFromHTML error: ", rawCurrentURL, err)
+		}
+		return
 	}
 
 	// go deeper
+	func() { <-cfg.concurrencyControl }()
 	for _, v := range gotURLs {
-		log.Printf("   width  : \"%s\"", v)
-		crawlPage(rawBaseURL, v, pages, currentDepth, maxDepth)
+		cfg.concurrencyControl <- struct{}{}
+		cfg.wg.Add(1)
+
+		if cfg.lo.doLogging && cfg.lo.doWidth {
+			log.Printf("   width  : \"%s\"", v)
+		}
+		go cfg.internalCrawlPage(v, currentDepth)
 	}
+	cfg.concurrencyControl <- struct{}{}
 }
--- main.go
+++ main.go
@@ -2,24 +2,89 @@ package main
 
 import (
 	"fmt"
-	"io"
 	"log"
+	"net/url"
 	"os"
+	"strings"
+	"sync"
+	"time"
 )
 
-func main() {
-	// setup logging output
-	logFile, err := os.OpenFile("app.log", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
+type loggingOptions struct {
+	doLogging   bool
+	doStart     bool
+	doEnd       bool
+	doSummary   bool
+	doAbyss     bool
+	doDepth     bool
+	doWidth     bool
+	doErrors    bool
+	doPages     bool
+	logsFolder  string
+	doIdRoutine bool
+	logName     string
+	dateSuffix  string
+	logToFile   bool
+	logToScreen bool
+}
+
+func (lo *loggingOptions) setDefultLoggingOptions() {
+	lo.doLogging = true
+	lo.doStart = true
+	lo.doEnd = true
+	lo.doSummary = true
+	lo.doAbyss = false
+	lo.doWidth = true
+	lo.doDepth = true
+	lo.doErrors = true
+	lo.doPages = true // not implemented
+
+	lo.logToFile = false
+	lo.logToScreen = true
+	lo.logsFolder = "/tmp/protoCrawler/logs"
+	lo.logName = "crawler"
+	lo.dateSuffix = "2006-01-02_15:04:05"
+	lo.doIdRoutine = true // not implemented
+}
+func (lo *loggingOptions) getLogFile() (lf *os.File) {
+	var logFileName string
+	if lo.dateSuffix != "" {
+		currentTime := time.Now()
+		tf := currentTime.Format(lo.dateSuffix)
+
+		logFileName = fmt.Sprintf("%s_%s.log", lo.logName, tf)
+	} else {
+		logFileName = fmt.Sprintf("%s.log", lo.logName)
+	}
+
+	lo.logsFolder = strings.TrimRight(lo.logsFolder, "/")
+	err := os.MkdirAll(lo.logsFolder, 0755)
+	if err != nil {
+		fmt.Printf("Error creating directory: %v\n", err)
+		return
+	}
+
+	logPath := fmt.Sprintf("%s/%s", lo.logsFolder, logFileName)
+	logFile, err := os.OpenFile(logPath, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
 	if err != nil {
 		log.Fatalf("Failed to open log file: %v", err)
 	}
-	defer logFile.Close()
+	return logFile
+}
 
-	multiWriter := io.MultiWriter(os.Stdout, logFile)
-	log.SetOutput(multiWriter)
+type config struct {
+	pages              map[string]int
+	mu                 *sync.Mutex // mutex for pages
+	baseURL            *url.URL
+	concurrencyControl chan struct{}
+	wg                 *sync.WaitGroup
+	maxPages           uint
+	maxDepth           uint
+	lo                 *loggingOptions
+	lf                 *os.File
+}
 
-	// main
-
+func main() {
 	if len(os.Args) <= 1 {
 		fmt.Println("no website provided")
 		os.Exit(1)
@@ -29,20 +94,30 @@ func main() {
 		fmt.Println("too many arguments provided")
 		os.Exit(1)
 	}
-	baseUrl := os.Args[1]
-	maxDepth := 0 // dept 0 = inf
-	log.Printf("starting crawl of: \"%s\" with max depth of %d\n", baseUrl, maxDepth)
-	log.Print("\n----------\n\n")
 
-	pages := make(map[string]int)
-	crawlPage(baseUrl, baseUrl, pages, 0, maxDepth)
-
-	log.Print("\n----------\n\n")
-	log.Println("end of crawl")
-	log.Print("\n----------\n\n")
-
-	log.Println("Contents of the pages map:")
-	for url, n := range pages {
-		log.Printf("%s ||| N: %d\n", url, n)
+	baseUrlStr := os.Args[1]
+	cfg := config{}
+	cfg.mu = &sync.Mutex{}
+	cfg.pages = make(map[string]int)
+	cfg.wg = &sync.WaitGroup{}
+	var err error
+	cfg.baseURL, err = url.Parse(baseUrlStr)
+	if err != nil {
+		log.Fatalf("Base url parse error: %v", err) // is this correct?
+		os.Exit(1)
 	}
+
+	// setup logging
+	cfg.lo = &loggingOptions{}
+	cfg.lo.setDefultLoggingOptions()
+
+	cfg.lo.doSummary = false
+	cfg.lo.logToFile = false
+	cfg.lo.logToScreen = true
+	cfg.concurrencyControl = make(chan struct{}, 9)
+	cfg.maxDepth = 3
+	cfg.maxPages = 50
+
+	// John Crawler
+	cfg.crawlPage(baseUrlStr)
 }
